eef6ed7a1590f773f0ca685287fadd16cefe9e18
YuseiMorimoto <Yusei0509@icloud.com>
2024-05-16 23:42:07 +0900

    ✨ sidebarのUIを作成

---
+++ src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,45 @@
+import "./Sidebar.css";
+import { assets } from "../../assets/assets";
+
+const Sidebar = () => {
+  return (
+    <div className="sidebar">
+      <div className="top">
+        <img className="menu" src={assets.menu_icon} alt="" />
+        <div className="new-chat">
+          <img src={assets.plus_icon} alt="" />
+          <p>New Chat</p>
+        </div>
+        <div className="recent">
+          <p className="recent-title">Recent</p>
+          <div className="recent-entry">
+            <img src={assets.message_icon} alt="" />
+            <p>What is React...</p>
+          </div>
+        </div>
+      </div>
+
+      <div className="bottom">
+        {/* Help */}
+        <div className="bottom-item recent-entry">
+          <img src={assets.question_icon} alt="" />
+          <p>Help</p>
+        </div>
+
+        {/* 履歴 */}
+        <div className="bottom-item recent-entry">
+          <img src={assets.history_icon} alt="" />
+          <p>Activity</p>
+        </div>
+
+        {/* 設定 */}
+        <div className="bottom-item recent-entry">
+          <img src={assets.setting_icon} alt="" />
+          <p>Settings</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
