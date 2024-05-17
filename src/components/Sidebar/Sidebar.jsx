b55bd5bfefadb150b185b15dc49ab554610aa57a
YuseiMorimoto <Yusei0509@icloud.com>
2024-05-17 11:46:32 +0900

    ✨ サイドバーの開閉機能を作成

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,41 +1,58 @@
 import "./Sidebar.css";
 import { assets } from "../../assets/assets";
+import { useState } from "react";
 
 const Sidebar = () => {
+  const [extended, setExtended] = useState(false);
+
+  const handleChange = () => {
+    setExtended((prev) => !prev);
+  };
+
   return (
     <div className="sidebar">
       <div className="top">
-        <img className="menu" src={assets.menu_icon} alt="" />
+        {/* ハンバーガーメニュー */}
+        <img
+          onClick={handleChange}
+          className="menu"
+          src={assets.menu_icon}
+          alt=""
+        />
         <div className="new-chat">
           <img src={assets.plus_icon} alt="" />
-          <p>New Chat</p>
+          {extended ? <p>New Chat</p> : null}
         </div>
-        <div className="recent">
-          <p className="recent-title">Recent</p>
-          <div className="recent-entry">
-            <img src={assets.message_icon} alt="" />
-            <p>What is React...</p>
+
+        {/* Recent */}
+        {extended ? (
+          <div className="recent">
+            <p className="recent-title">Recent</p>
+            <div className="recent-entry">
+              <img src={assets.message_icon} alt="" />
+              <p>What is React...</p>
+            </div>
           </div>
-        </div>
+        ) : null}
       </div>
 
       <div className="bottom">
         {/* Help */}
         <div className="bottom-item recent-entry">
           <img src={assets.question_icon} alt="" />
-          <p>Help</p>
+          {extended ? <p>Help</p> : null}
         </div>
 
         {/* 履歴 */}
         <div className="bottom-item recent-entry">
           <img src={assets.history_icon} alt="" />
-          <p>Activity</p>
+          {extended ? <p>Activity</p> : null}
         </div>
 
         {/* 設定 */}
         <div className="bottom-item recent-entry">
           <img src={assets.setting_icon} alt="" />
-          <p>Settings</p>
+          {extended ? <p>Settings</p> : null}
         </div>
       </div>
     </div>
